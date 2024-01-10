072c97cf55c0ae2e1e8a77fd803a13a70d27db36
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2024-01-10 19:54:04 -0300

    add config mfe 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     mfe1Url:'https://mfe-1-nine.vercel.app',
-    mfe2Url:'https://mfe-2-blond.vercel.app'
+    mfe2Url:'https://mfe-2-blond.vercel.app',
 };
